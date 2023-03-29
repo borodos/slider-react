57e614c9299251971d5b862f4fb049d9adfbaeb7
Georgii <87697829+borodos@users.noreply.github.com>
2023-03-30 02:15:49 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// напиши hello world на C++ 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
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
